edd8b86cdecd95fadfbed964ecae313f2e42d6b5
Hema Anand V <hema.v@stackroute.in>
2020-01-03 14:47:21 +0530

    Changes made

--- src/main/java/com/stackroute/basics/SumOfCommonElements.java
+++ src/main/java/com/stackroute/basics/SumOfCommonElements.java
@@ -1,61 +1,28 @@
 package com.stackroute.basics;
 
-import java.util.Scanner;
-
 public class SumOfCommonElements {
-    Scanner scanner = new Scanner(System.in);
+    //Create Scanner object as instance variable
 
     public static void main(String[] args) {
-        SumOfCommonElements object = new SumOfCommonElements();
+        //Get the size for the arrays
 
-        int size = object.getArraySize();
-
-        int[] firstArray = object.getArrayElements(size);
-        int[] secondArray = object.getArrayElements(size);
-
-        int result = object.calculateSumOfCommonElements(firstArray, secondArray);
-
-        object.displayResult(result);
+        //Initialize the array elements using getArrayElements() method
     }
 
     public int getArraySize() {
-        return scanner.nextInt();
+        return 0;
     }
 
     public int[] getArrayElements(int size) {
-        int[] array = new int[size];
-        for (int i = 0; i < size; i++) {
-          array[i] = scanner.nextInt();
-        }
-        return array;
+        return null;
     }
 
     public void displayResult(int result) {
-        if(result==-1) {
-            System.out.println("No elements in arrays");
-        }
-        else{
-            System.out.println(result);
-        }
-
+        //display the result
     }
 
     public int calculateSumOfCommonElements(int firstArray[], int secondArray[]) {
-        if(firstArray.length==0 ||secondArray.length==0){
-            return -1;
-        }
-
-        int sum = 0;
-        for (int i = 0; i < firstArray.length; i++) {
-            for (int j = 0; j < secondArray.length; j++) {
-                if (firstArray[i] == secondArray[j])
-                    sum = sum + firstArray[i];
-            }
-        }
-        if (sum == 0) {
-            return 0;
-        } else {
-            return sum;
-        }
+        return 0;
     }
+
 }
---
+++ src/test/java/com/stackroute/basics/commander/test/SumOfCommonElementsTests.java
@@ -0,0 +1,46 @@
+package com.stackroute.basics.commander.test;
+
+import com.stackroute.basics.SumOfCommonElements;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class SumOfCommonElementsTests {
+    private static final String messageOne = "Should return sum of the common elements in two arrays";
+    private static final String messageTwo = "Should return 0 if there are no common elements";
+    private static SumOfCommonElements object;
+
+    @BeforeEach
+    public static void setup() {
+        // This methods runs, before running each test case
+        // This method is used to initialize the required variables
+        object = new SumOfCommonElements();
+    }
+
+    @AfterEach
+    public static void teardown() {
+        // This method runs, after each test case
+        // This method is used to clear the initialized variables
+        object = null;
+    }
+
+    @Test
+    public void givenTwoArraysWithValidSizeHavingCommonElementsWhenCommonElementsFoundThenMethodReturnsTheirSum() {
+        int[] arrayOne = {10, 20, 30, 40, 50};
+        int[] arrayTwo = {5, 10, 15, 20, 25};
+        int sumExpected = 30;
+        int sumActual = object.calculateSumOfCommonElements(arrayOne, arrayTwo);
+        Assertions.assertEquals(sumExpected, sumActual, messageOne);
+    }
+
+    @Test
+    public void givenTwoArraysWithValidSizeHavingUncommonElementsWhenCommonElementsNotFoundThenMethodReturnsZero() {
+        int[] arrayOne = {10, 20, 30, 40, 50};
+        int[] arrayTwo = {1, 2, 3, 4, 5};
+        int sumExpected = 0;
+        int sumActual = object.calculateSumOfCommonElements(arrayOne, arrayTwo);
+        Assertions.assertEquals(sumExpected, sumActual,messageTwo);
+    }
+}
